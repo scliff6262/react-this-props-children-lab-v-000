81a0ac9e0ae56576b8fb7568c190fa08014e8d89
scliff6262 <sclifford6262@gmail.com>
2018-07-05 14:27:11 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,16 @@
-// Code Invitation Component Here
+import React from 'react'
+
+class Invitation extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>You've been invited!</h1>
+        <div>
+        {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Invitation
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,22 @@
-// Code ThemedDecoration Component Here
+import React from 'react'
+
+class ThemedDecorations extends React.Component {
+  render(){
+    function assignedTheme(props){
+      let newElements = []
+      React.Children.forEach(props.children, function(child){
+        const newProps = Object.assign({}, child.props)
+        newProps["className"] = props.theme
+        newElements.push(React.cloneElement(child, newProps))
+    })
+    return newElements
+  }
+    return(
+      <div className={this.props.theme}>
+        {assignedTheme(this.props)}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecorations
